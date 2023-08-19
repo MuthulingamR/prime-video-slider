8015cd609e75ace2fc8309582eae18d4ddba81bd
MuthulingamR <muthulingam1410@gmail.com>
2023-08-19 13:46:02 +0000

    Prime Silder

---
+++ src/components/PrimeVideo/index.js
@@ -0,0 +1,30 @@
+import MoviesSlider from '../MoviesSlider'
+import './index.css'
+
+const PrimeVideo = props => {
+  const {moviesList} = props
+  const actionMovieList = moviesList.filter(
+    each => each.categoryId === 'ACTION',
+  )
+  const comedyMovieList = moviesList.filter(
+    each => each.categoryId !== 'ACTION',
+  )
+
+  return (
+    <div className="prime-video-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/prime-video-img.png"
+        alt="prime video"
+        className="prime-image"
+      />
+      <div className="movies-container">
+        <h1 className="movie-heading">Action Movies</h1>
+        <MoviesSlider moviesList={actionMovieList} />
+        <h1 className="movie-heading">Comedy Movies</h1>
+        <MoviesSlider moviesList={comedyMovieList} />
+      </div>
+    </div>
+  )
+}
+
+export default PrimeVideo
